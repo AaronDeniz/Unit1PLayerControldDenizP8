fb4530d784f01dd414f57792a979d422d57a52a1
Aaron  Deniz <145504641+AaronDeniz@users.noreply.github.com>
2024-03-05 15:28:43 -0800

    day 4
    added oncoming cars and started adding new camera views

--- Unit1PlayerControl/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControl/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +28,14 @@ public class PlayerController : MonoBehaviour
 
         // We move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       
         // We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
